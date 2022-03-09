01e61784e8aa3df4e0dcab164c05f277e8913cb0
chrislupdx <christopherlu.pdx@gmail.com>
2022-03-08 16:53:32 -0800

    prodcon 2 works

--- prodcon2.c
+++ prodcon2.c
@@ -10,8 +10,9 @@ typedef struct {
     int count;
 } myargs_t;
 pthread_mutex_t producer_mutex; 
-pthread_mutex_t consumer_mutex; 
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER; //DOES THIS HAVE A DONE VAR INSDIE IT:w
+//pthread_mutex_t consumer_mutex; 
+pthread_cond_t  fill = PTHREAD_COND_INITIALIZER; //DOES THIS HAVE A DONE VAR INSDIE IT
+pthread_cond_t  empty = PTHREAD_COND_INITIALIZER; //DOES THIS HAVE A DONE VAR INSDIE IT
 //figure out the states we can toggle in cond_t
 
 void *producer_f(void * arg);
@@ -20,7 +21,7 @@ void *consumer_f(void * arg);
 int main()
 {
     int count = 10;
-    int rc = pthread_mutex_init(&consumer_mutex, NULL);
+    //int rc = pthread_mutex_init(&consumer_mutex, NULL);
     int rc2 = pthread_mutex_init(&producer_mutex, NULL);
     char * buffer = (char *)malloc(256 * sizeof(char));  //this is what we're going to pass back and forth between producer_f and consumer_f
     char * input = (char *)malloc(256 * sizeof(char));  //this is used to 
@@ -51,6 +52,12 @@ void *producer_f(void * arg)
 {
     pthread_mutex_lock(&producer_mutex);
     myargs_t *args = (myargs_t *) arg;
+    //while the buffer is full, wait.
+    while(strlen(args->buffer) > 0)
+    {
+        //printf("inside while arg->buffer %s\n", args->buffer);
+        pthread_cond_wait(&empty, &producer_mutex);
+    }
     FILE * pFile;
     char fform[50] = "txts/in"; //this is  missing the # that we may iterate through
     char curr[50];
@@ -68,15 +75,23 @@ void *producer_f(void * arg)
         args->buffer[256] = '\0';
     }
     (args->count)++;
+    pthread_cond_signal(&fill);
     pthread_mutex_unlock(&producer_mutex);
     return NULL;
 }
+
 void *consumer_f(void * arg)
 {
     pthread_mutex_lock(&producer_mutex);
     myargs_t * args = (myargs_t *) arg;
-    //printf("count is %d buffer is %s \n", args->count, args->buffer);
+    //while the buffer is empty, wait
+    while(strlen(args->buffer) == 0) 
+    {
+        pthread_cond_wait(&fill, &producer_mutex);
+    }
     printf("%s \n", args->buffer);
+    args->buffer[0] = 0;
+    pthread_cond_signal(&empty); //signals buffer is empty+ready
     pthread_mutex_unlock(&producer_mutex);
     return NULL;
 }
